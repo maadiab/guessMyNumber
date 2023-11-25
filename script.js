4440869422d0fb3c369474aa1ae7fa67a5e2b496
maadiab <mohanad.dev@gmail.com>
2023-11-25 20:53:15 +0300

    some codes

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number';
+// console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 13;
