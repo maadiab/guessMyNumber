845b06b30fb904a0f192a13d0d702c9acebe55f7
maadiab <mohanad.dev@gmail.com>
2023-11-26 21:45:52 +0300

    make game logic

--- script.js
+++ script.js
@@ -5,6 +5,11 @@
 // document.querySelector('.number').textContent = 13;
 
 // document.querySelector('.score').textContent = 13;
+let score = 20;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,5 +17,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🎉 great! Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🦒 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = '💣 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
